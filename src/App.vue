67ab940fcf3cf38f85f8477bf99be9ca56a62dd6
David Andersson <david.andersson@zocom.se>
2021-09-09 11:45:49 +0200

    Parent and child components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
+    <new-component v-bind:price="thePrice"
+        v-on:innerClick="handleClickFromNewComponent"></new-component>
+
+    {{ message }}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NewComponent from './components/NewComponent.vue'
+// .    <- current directory
+// ..   <- parent directory
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        HelloWorld,
+        "new-component": NewComponent
+    },
+    data: () => ({
+        thePrice: 256,
+        message: 'No click recorded'
+    }),
+    methods: {
+        handleClickFromNewComponent(data) {
+            this.message = 'Click recorded, with data: ' + data.message
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/NewComponent.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="new-component">
+		<h1> I am the new component </h1>
+		<p>
+			The price is {{ price }} SEK.
+		</p>
+		<button v-on:click="handleClick"> Click me </button>
+	</div>
+	<!-- <p>
+		Man ska bara ha ett element direkt i en template
+	</p> -->
+</template>
+
+<script>
+export default {
+	// Lägg till data, computed, methods, props vid behov
+	props: ['price'],
+	// det blir som om vi hade en data property "price"
+
+	methods: {
+		handleClick() {
+			// Tips för felsökning: ta reda på om koden körs
+			console.log('NewComponent: handleClick');
+
+			const data = { message: 'Success!' }
+			this.$emit('innerClick', data)
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+.new-component {
+	border: 1px solid gray;
+	padding: 1em;
+	margin: 1em;
+	border-radius: 1em;
+}
+h1 {
+	color: hotpink;
+}
+</style>
